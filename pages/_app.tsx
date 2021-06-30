78cfbaf20b39b10e323f449d4dc45dcc0f0287b8
Mykhailo Pliskunov <mykhailo.pliskunov@gmail.com>
2021-06-30 15:39:04 +0300

    header and document attributes rewriting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+	<Head>
+		<title>MyTop = our best top</title>
+		<link key={1} rel="icon" href="/favicon2.ico" />
+	</Head>
+	<Component {...pageProps} />
+  </>
+  ;
 }
 
 export default MyApp;
